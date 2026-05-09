9c689a36f03f3c7c692f6e2d307bec47eb550c08
topkekhap1234 <topkekhap1234@gmail.com>
2026-05-09 19:05:46 +0000

    нормальный вфс, вместо condition иноды, убрано тотал и юзд мемори

--- vfs.c
+++ vfs.c
@@ -8,39 +8,20 @@
 #include <sys/statvfs.h>
 
 #define MOUNT "/"
-#define LIMIT 90.0
 
 int main(void) {
     struct statvfs s;
     if (statvfs(MOUNT, &s) != 0) {
-        perror("statvfs error");
+        perror("root error");
         return 1;
     }
 
-    unsigned long tot = (s.f_blocks * s.f_frsize) / 1024 / 1024;
-    unsigned long avl = (s.f_bavail * s.f_frsize) / 1024 / 1024;
-    double use = 100.0 - ((double)s.f_bavail / s.f_blocks * 100.0);
-    
-    // Считаем нагрузку на иноды (файловые дескрипторы)
+    double use = 100.0 - ((double)s.f_bavail / s.f_blocks * 100.0);    
     double i_use = 100.0 - ((double)s.f_favail / s.f_files * 100.0);
-    double worst = (use > i_use) ? use : i_use;
 
-    printf("%-10s %-10s %-10s %-10s %-10s\n", "MOUNT", "TOTAL_MB", "FREE_MB", "USED_%", "CONDITION");
+    printf("%-10s %-10s %-10s\n", "MOUNT",  "USED_MEM%", "USED_INODES_%");
 
-    printf("%-10s %-10lu %-10lu %-10.1f ", MOUNT, tot, avl, use);
-
-    // Логика умного CONDITION вместо STATUS
-    if (worst > 98.0) {
-        printf("%-10s\n", "FULL");
-    } else if (worst > LIMIT) {
-        printf("%-10s\n", "CRITICAL");
-    } else if (worst > 75.0) {
-        printf("%-10s\n", "CROWDED");
-    } else if (s.f_flag & ST_RDONLY) {
-        printf("%-10s\n", "READONLY");
-    } else {
-        printf("%-10s\n", "HEALTHY");
-    }
+    printf("%-10s %-10.1f %-10.1f\n", MOUNT, use, i_use);
 
     return 0;
 }
